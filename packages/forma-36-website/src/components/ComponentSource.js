323496cf198bf9daca87d531bea1b51d64db9da3
burakukula <trycjapa@gmail.com>
2020-06-11 13:39:51 +0200

    feat: Extended docs POC

--- packages/forma-36-website/src/components/ComponentSource.js
+++ packages/forma-36-website/src/components/ComponentSource.js
@@ -3,6 +3,7 @@ import { css } from '@emotion/core';
 import tokens from '@contentful/forma-36-tokens';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import * as f36Components from '@contentful/forma-36-react-components';
+import { Autocomplete, Grid, GridItem } from '@contentful/forma-36-react-components/dist/alpha';
 import { Card, Button } from '@contentful/forma-36-react-components';
 
 const styles = {
@@ -36,7 +37,7 @@ class ComponentSource extends React.Component {
   render() {
     return (
       <div className="f36-margin-bottom--m">
-        <LiveProvider code={this.props.children} scope={{ ...f36Components }}>
+        <LiveProvider code={this.props.children} scope={{ ...f36Components, Grid, Autocomplete, GridItem }}>
           <Card padding="none">
             <div css={styles.preview}>
               <LivePreview />
