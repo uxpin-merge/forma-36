1702d9ccf9b9b3d3f992dcb88d99fb525d3cb0c3
sam moore <swmoore84@gmail.com>
2021-05-21 12:37:46 +0900

    create TextField preset

---
+++ packages/forma-36-react-components/src/components/TextField/presets/0-default.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextField } from '../TextField';
+
+export default (
+  <TextField
+    uxpId="text-field-1"
+    width="full"
+    labelText="Email address"
+    helpText="Please enter your email address"
+    required={true}
+  />
+);
