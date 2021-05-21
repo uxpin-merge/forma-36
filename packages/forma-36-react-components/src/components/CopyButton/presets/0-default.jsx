c674ab41e79e43a548d65f3494ffd72f4539fa8c
sam moore <swmoore84@gmail.com>
2021-05-21 16:45:40 +0900

    add copy button and presets

---
+++ packages/forma-36-react-components/src/components/CopyButton/presets/0-default.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CopyButton } from '../CopyButton';
+
+export default (
+  <CopyButton
+    uxpId="copy-button-1"
+    copyValue="Lorem Ipsum"
+    testId="cf-ui-copy-button"
+    tooltipPlace="auto"
+    tooltipText="Copy this text to clipboard"
+    tooltipCopiedText="The text has been copied"
+  />
+);
