afa3eb57d5bbd98c477f051de75f6c267a28e6aa
sam moore <swmoore84@gmail.com>
2021-05-21 16:56:57 +0900

    add spinner

---
+++ packages/components/spinner/src/presets/0-default.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Spinner } from '../Spinner';
+
+export default (
+  <Spinner
+    uxpId="spinner-1"
+    variant="default"
+    customSize={1}
+    size="medium"
+    display="-moz-initial"
+  >Put Spinner contents here</Spinner>
+);
