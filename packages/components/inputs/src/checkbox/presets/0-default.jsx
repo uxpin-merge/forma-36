600d1d264bbf5f5126b45f518d732e842ed6bdbe
sam moore <swmoore84@gmail.com>
2021-05-25 16:54:06 +0900

    add checkbox and presets

---
+++ packages/components/inputs/src/checkbox/presets/0-default.jsx
@@ -0,0 +1,285 @@
+import React from 'react';
+import { Checkbox } from '../Checkbox';
+
+export default (
+  <Checkbox
+    uxpId="checkbox-1"
+    id=""
+    isRequired={false}
+    label=""
+    isChecked={false}
+    onChange={() => {}}
+    name=""
+    onBlur={() => {}}
+    onFocus={() => {}}
+    value=""
+    isDisabled={false}
+    type="checkbox"
+    className=""
+    testId="cf-ui-checkbox"
+    canBlurOnEsc={false}
+    isIndeterminate={false}
+    inputProps=""
+    slot=""
+    style={{}}
+    title=""
+    as={null}
+    key=""
+    defaultChecked={false}
+    defaultValue=""
+    suppressContentEditableWarning={false}
+    suppressHydrationWarning={false}
+    accessKey=""
+    contentEditable={false}
+    contextMenu=""
+    dir=""
+    draggable={false}
+    hidden={false}
+    lang=""
+    placeholder=""
+    spellCheck={false}
+    tabIndex={1}
+    translate="yes"
+    radioGroup=""
+    role=""
+    about=""
+    datatype=""
+    inlist=""
+    prefix=""
+    property=""
+    resource=""
+    typeof=""
+    vocab=""
+    autoCapitalize=""
+    autoCorrect=""
+    autoSave=""
+    color=""
+    itemProp=""
+    itemScope={false}
+    itemType=""
+    itemID=""
+    itemRef=""
+    results={1}
+    security=""
+    unselectable="on"
+    inputMode="text"
+    is=""
+    aria-activedescendant=""
+    aria-atomic={false}
+    aria-autocomplete="none"
+    aria-busy={false}
+    aria-checked={false}
+    aria-colcount={1}
+    aria-colindex={1}
+    aria-colspan={1}
+    aria-controls=""
+    aria-current={false}
+    aria-describedby=""
+    aria-details=""
+    aria-disabled={false}
+    aria-dropeffect="link"
+    aria-errormessage=""
+    aria-expanded={false}
+    aria-flowto=""
+    aria-grabbed={false}
+    aria-haspopup={false}
+    aria-hidden={false}
+    aria-invalid={false}
+    aria-keyshortcuts=""
+    aria-label=""
+    aria-labelledby=""
+    aria-level={1}
+    aria-live="off"
+    aria-modal={false}
+    aria-multiline={false}
+    aria-multiselectable={false}
+    aria-orientation="horizontal"
+    aria-owns=""
+    aria-placeholder=""
+    aria-posinset={1}
+    aria-pressed={false}
+    aria-readonly={false}
+    aria-relevant="text"
+    aria-required={false}
+    aria-roledescription=""
+    aria-rowcount={1}
+    aria-rowindex={1}
+    aria-rowspan={1}
+    aria-selected={false}
+    aria-setsize={1}
+    aria-sort="none"
+    aria-valuemax={1}
+    aria-valuemin={1}
+    aria-valuenow={1}
+    aria-valuetext=""
+    dangerouslySetInnerHTML={{}}
+    onCopy={() => {}}
+    onCopyCapture={() => {}}
+    onCut={() => {}}
+    onCutCapture={() => {}}
+    onPaste={() => {}}
+    onPasteCapture={() => {}}
+    onCompositionEnd={() => {}}
+    onCompositionEndCapture={() => {}}
+    onCompositionStart={() => {}}
+    onCompositionStartCapture={() => {}}
+    onCompositionUpdate={() => {}}
+    onCompositionUpdateCapture={() => {}}
+    onFocusCapture={() => {}}
+    onBlurCapture={() => {}}
+    onChangeCapture={() => {}}
+    onBeforeInput={() => {}}
+    onBeforeInputCapture={() => {}}
+    onInput={() => {}}
+    onInputCapture={() => {}}
+    onReset={() => {}}
+    onResetCapture={() => {}}
+    onSubmit={() => {}}
+    onSubmitCapture={() => {}}
+    onInvalid={() => {}}
+    onInvalidCapture={() => {}}
+    onLoad={() => {}}
+    onLoadCapture={() => {}}
+    onError={() => {}}
+    onErrorCapture={() => {}}
+    onKeyDown={() => {}}
+    onKeyDownCapture={() => {}}
+    onKeyPress={() => {}}
+    onKeyPressCapture={() => {}}
+    onKeyUp={() => {}}
+    onKeyUpCapture={() => {}}
+    onAbort={() => {}}
+    onAbortCapture={() => {}}
+    onCanPlay={() => {}}
+    onCanPlayCapture={() => {}}
+    onCanPlayThrough={() => {}}
+    onCanPlayThroughCapture={() => {}}
+    onDurationChange={() => {}}
+    onDurationChangeCapture={() => {}}
+    onEmptied={() => {}}
+    onEmptiedCapture={() => {}}
+    onEncrypted={() => {}}
+    onEncryptedCapture={() => {}}
+    onEnded={() => {}}
+    onEndedCapture={() => {}}
+    onLoadedData={() => {}}
+    onLoadedDataCapture={() => {}}
+    onLoadedMetadata={() => {}}
+    onLoadedMetadataCapture={() => {}}
+    onLoadStart={() => {}}
+    onLoadStartCapture={() => {}}
+    onPause={() => {}}
+    onPauseCapture={() => {}}
+    onPlay={() => {}}
+    onPlayCapture={() => {}}
+    onPlaying={() => {}}
+    onPlayingCapture={() => {}}
+    onProgress={() => {}}
+    onProgressCapture={() => {}}
+    onRateChange={() => {}}
+    onRateChangeCapture={() => {}}
+    onSeeked={() => {}}
+    onSeekedCapture={() => {}}
+    onSeeking={() => {}}
+    onSeekingCapture={() => {}}
+    onStalled={() => {}}
+    onStalledCapture={() => {}}
+    onSuspend={() => {}}
+    onSuspendCapture={() => {}}
+    onTimeUpdate={() => {}}
+    onTimeUpdateCapture={() => {}}
+    onVolumeChange={() => {}}
+    onVolumeChangeCapture={() => {}}
+    onWaiting={() => {}}
+    onWaitingCapture={() => {}}
+    onAuxClick={() => {}}
+    onAuxClickCapture={() => {}}
+    onClick={() => {}}
+    onClickCapture={() => {}}
+    onContextMenu={() => {}}
+    onContextMenuCapture={() => {}}
+    onDoubleClick={() => {}}
+    onDoubleClickCapture={() => {}}
+    onDrag={() => {}}
+    onDragCapture={() => {}}
+    onDragEnd={() => {}}
+    onDragEndCapture={() => {}}
+    onDragEnter={() => {}}
+    onDragEnterCapture={() => {}}
+    onDragExit={() => {}}
+    onDragExitCapture={() => {}}
+    onDragLeave={() => {}}
+    onDragLeaveCapture={() => {}}
+    onDragOver={() => {}}
+    onDragOverCapture={() => {}}
+    onDragStart={() => {}}
+    onDragStartCapture={() => {}}
+    onDrop={() => {}}
+    onDropCapture={() => {}}
+    onMouseDown={() => {}}
+    onMouseDownCapture={() => {}}
+    onMouseEnter={() => {}}
+    onMouseLeave={() => {}}
+    onMouseMove={() => {}}
+    onMouseMoveCapture={() => {}}
+    onMouseOut={() => {}}
+    onMouseOutCapture={() => {}}
+    onMouseOver={() => {}}
+    onMouseOverCapture={() => {}}
+    onMouseUp={() => {}}
+    onMouseUpCapture={() => {}}
+    onSelect={() => {}}
+    onSelectCapture={() => {}}
+    onTouchCancel={() => {}}
+    onTouchCancelCapture={() => {}}
+    onTouchEnd={() => {}}
+    onTouchEndCapture={() => {}}
+    onTouchMove={() => {}}
+    onTouchMoveCapture={() => {}}
+    onTouchStart={() => {}}
+    onTouchStartCapture={() => {}}
+    onPointerDown={() => {}}
+    onPointerDownCapture={() => {}}
+    onPointerMove={() => {}}
+    onPointerMoveCapture={() => {}}
+    onPointerUp={() => {}}
+    onPointerUpCapture={() => {}}
+    onPointerCancel={() => {}}
+    onPointerCancelCapture={() => {}}
+    onPointerEnter={() => {}}
+    onPointerEnterCapture={() => {}}
+    onPointerLeave={() => {}}
+    onPointerLeaveCapture={() => {}}
+    onPointerOver={() => {}}
+    onPointerOverCapture={() => {}}
+    onPointerOut={() => {}}
+    onPointerOutCapture={() => {}}
+    onGotPointerCapture={() => {}}
+    onGotPointerCaptureCapture={() => {}}
+    onLostPointerCapture={() => {}}
+    onLostPointerCaptureCapture={() => {}}
+    onScroll={() => {}}
+    onScrollCapture={() => {}}
+    onWheel={() => {}}
+    onWheelCapture={() => {}}
+    onAnimationStart={() => {}}
+    onAnimationStartCapture={() => {}}
+    onAnimationEnd={() => {}}
+    onAnimationEndCapture={() => {}}
+    onAnimationIteration={() => {}}
+    onAnimationIterationCapture={() => {}}
+    onTransitionEnd={() => {}}
+    onTransitionEndCapture={() => {}}
+    display="ruby"
+    margin="none"
+    marginTop="none"
+    marginRight="none"
+    marginBottom="none"
+    marginLeft="none"
+    padding="none"
+    paddingTop="none"
+    paddingRight="none"
+    paddingBottom="none"
+    paddingLeft="none"
+  >Put Checkbox contents here</Checkbox>
+);
