fb731165dae9774124563d73e86a68b37359d7b9
sam moore <swmoore84@gmail.com>
2021-05-25 16:55:15 +0900

    add card presets

---
+++ packages/forma-36-react-components/src/components/Card/presets/0-default.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Card } from '../Card';
+
+export default (
+  <Card
+    uxpId="card-1"
+    onClick={() => {}}
+    padding="default"
+    title="Title"
+    style=""
+    ariaLabel=""
+    href=""
+    rel="noreferrer"
+    target=""
+    selected={false}
+    testId="cf-ui-card"
+    className=""
+  >Put Card contents here</Card>
+);
