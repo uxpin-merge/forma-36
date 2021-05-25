0b8bf6f336b4bd8a368409bb7cb45625edf5b94b
sam moore <swmoore84@gmail.com>
2021-05-25 16:49:26 +0900

    add radio button and presets

---
+++ packages/components/inputs/src/radio-button/presets/0-default.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { RadioButton } from '../RadioButton';
+
+export default (
+  <RadioButton
+    uxpId="radio-button-1"
+    id="radio"
+    name="someOption"
+  >Put RadioButton contents here</RadioButton>
+);
