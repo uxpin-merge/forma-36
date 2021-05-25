9852b5d3326a0aaf8015ceb653d134d40c18ac32
sam moore <swmoore84@gmail.com>
2021-05-25 16:12:19 +0900

    updated preset file for tooltip

---
+++ packages/forma-36-react-components/src/components/Tooltip/presets/0-default.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Tooltip } from '../Tooltip';
+
+export default (
+  <Tooltip
+    uxpId="tooltip-1"
+    content="Hi I am a Tooltip"
+    id="tip1"
+    place="top"
+    targetWrapperClassName="targetWrapperClassName"
+    testId="cf-ui-tooltip"
+  >Put Tooltip contents here</Tooltip>
+);
