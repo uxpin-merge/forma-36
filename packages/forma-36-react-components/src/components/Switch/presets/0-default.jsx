ce7af32e95e13c3abc778030af37f26746839ce0
sam moore <swmoore84@gmail.com>
2021-05-25 16:56:01 +0900

    add switch presets

---
+++ packages/forma-36-react-components/src/components/Switch/presets/0-default.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch } from '../Switch';
+
+export default (
+  <Switch
+    uxpId="switch-1"
+    labelText="label for switch"
+    id="testSwitch"
+  />
+);
