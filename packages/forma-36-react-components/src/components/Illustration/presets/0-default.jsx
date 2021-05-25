399d56a1f3d2e89c9035861a3d0b63c6e1560ad2
sam moore <swmoore84@gmail.com>
2021-05-25 16:30:42 +0900

    create preset for illustration

---
+++ packages/forma-36-react-components/src/components/Illustration/presets/0-default.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Illustration from '../Illustration';
+
+export default (
+  <Illustration
+    uxpId="illustration-1"
+    illustration="Archive"
+    testId="cf-ui-illustration"
+  />
+);
